0e4348743df1bac7e4f8d42ee8d36d47d387063b
Wabbadabba <shawn.wabschall@gmail.com>
2018-10-02 18:49:17 -0500

    Help File added

--- Get-Profit.ps1
+++ Get-Profit.ps1
@@ -1,19 +1,66 @@
-<#
-Roll 1d100 per (in-game) day to see how well the business does. Collect your earnings when you can. Maintainence cost is 5 gp/day.
 
-d100 - Result
-
-<20    - Pay 1 ½ x maintenance cost
-21-30 - Pay 1x maintenance cost
-31-40 - Pay ½x maintenance cost
-41-60 - Business covers its own maintenance cost
-61-80 - Business earns a profit of 1d6x5 gp
-81-90 - Business earns a profit of 2d8x5 gp
->91    - Business earns a profit of 3d10x5 gp
-#>
 
 
 function Get-Profit{
+<#
+.SYNOPSIS
+    Calculates total profit for a set number of days
+.DESCRIPTION
+    Rolls 1d100 per day as specified. The result determines an amount to either subtract from or add to the total. The total is then output to the screen
+
+    Roll Table Rules
+    ----------------
+    Roll 1d100 per (in-game) day to see how well the business does. 
+    Collect your earnings when you can. 
+    Maintainence cost is 5 gp/day. (Can be changed by altering $maint on LINE 92)
+
+    1d100:
+    <20    - Pay 1 ½ x maintenance cost
+    21-30 - Pay 1x maintenance cost
+    31-40 - Pay ½x maintenance cost
+    41-60 - Business covers its own maintenance cost
+    61-80 - Business earns a profit of 1d6x5 gp
+    81-90 - Business earns a profit of 2d8x5 gp
+    >91    - Business earns a profit of 3d10x5 gp
+.PARAMETER Days
+    Determines the number of days to calculate into the total
+.PARAMETER Full
+    Switch that shows roll output and subsequent currency calculation
+.EXAMPLE
+    PS C:\WINDOWS\system32> Get-Profit -days 2
+    
+    ---------------------------------------------------------------------
+    Calculating Profits for 1 Business for 2 days:
+    ---------------------------------------------------------------------
+
+    Profit for 2 days is: 5 gold
+    
+.EXAMPLE
+    PS C:\WINDOWS\system32> Get-Profit -days 2 -full
+    
+    ---------------------------------------------------------------------
+    Calculating Profits for 1 Business for 2 days:
+    ---------------------------------------------------------------------
+
+    rolling 1d100 : 82
+    Business earns a profit of 32 gold
+
+    rolling 1d100 : 83
+    Business earns a profit of 18 gold
+
+    Profit for 2 days is: 50 gold
+
+.NOTES
+    Author: Wabbadabba
+    ChangeLog
+    ---------
+    1Oct18 -- Script completed and uploaded to Github
+    2Oct18 -- Help comment added
+    
+    TODO
+    ----
+    - Add Silver/Copper conversion at final output
+#>
     param(
         [int]$days,
         [switch]$full
@@ -46,7 +93,9 @@ function Get-Profit{
     $totalDays = $days
     $total = 0 
     
+    # Change this to change maintanence cost
     $maint = 5
+    
     Clear-Host
     Write-Host $banner
     Write-Host "Calculating Profits for 1 Business for $totalDays days: `n
@@ -54,7 +103,6 @@ function Get-Profit{
     While($days -gt 0){
         $roll = get-random -min 1 -max 101
         
-
         if ($roll -lt 21){
             $cost = $maint * 1.5
             $total -= $maint
